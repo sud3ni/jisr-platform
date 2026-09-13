f072474c0b4501f2482b05f8fb3890201531eb57
sud3ni <mowfg.osmansuliman@gmail.com>
2026-09-13 15:27:38 +0200

    fix: update Supabase publishable key

--- supabase-server.ts
+++ supabase-server.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   {
     auth: {
       persistSession: false,
